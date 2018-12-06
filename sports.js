ba5a4266a6f2182363671bd4e335ce0e435f60b0
Stokris <kristen.stokes@me.com>
2018-12-06 12:12:22 -0500

    commit 12/6

--- sports.js
+++ sports.js
@@ -1,19 +1,19 @@
-const baseURL = "https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=Arsenal";
-const key = "1";
+const baseURL = "https://www.thesportsdb.com/api/v1/json/1/searchteams.php";
+const key = "4012948";
 let url;
 
 
 const searchTerm = document.querySelector('.search');
 const searchForm = document.querySelector('form');
 const submitBtn = document.querySelector('.submit');
+const section = document.querySelector('section');
 
 
-const nextBtn = document.querySelector('.next');
-const previousBtn = document.querySelector('.prev');
-const nav = document.querySelector('nav');
-
+// const nextBtn = document.querySelector('.next');
+// const previousBtn = document.querySelector('.prev');
+// const nav = document.querySelector('nav');
 let pageNumber = 0;
-console.log('PageNumber:', pageNumber);
+//console.log('PageNumber:', pageNumber);
 let displayNav = false;
 
 searchForm.addEventListener('submit', fetchResults);
@@ -23,12 +23,13 @@ searchForm.addEventListener('submit', fetchResults);
 
 function fetchResults(e) {
     e.preventDefault();
-   // url = baseURL + '?api-key=' + key + '&page=' + pageNumber + '&q=' + searchTerm.value;
-   url = baseURL;
-    console.log("URL:", url);
+     url = `${baseURL}?t=${searchTerm.value.split(" ").join("_")}`;
+    // url = baseURL + `${searchTerm.value}`;
+    console.log(url)
 
     fetch(url)
     .then(function(result) {
+        console.log(result);
         return result.json();
     }).then(function(json) {
         console.log(json);
@@ -37,36 +38,29 @@ function fetchResults(e) {
 }
 
 function displayResults(json) {
-    console.log(json);
-}
-
-
-
-
-
-
-
-
-
-nav.style.display = 'none';
-
-function nextPage(e) {
-    pageNumber++;
-    fetchResults(e);
-    console.log("Page number:", pageNumber);
-}
-
-function previousPage(e) {
-    if(pageNumber > 0) {
-        pageNumber--;
-    } else {
-        return;
+    while (section.firstChild) {
+        section.removeChild(section.firstChild);
     }
-    fetchResults(e);
-    console.log("Page:", pageNumber);
-};
 
-const section = document.querySelector('section');
+    let team = json.teams;
+
+    if (team.length === 0) {
+        console.log("No Results");
+    } else {
+        let team = json.teams[0];
+        //console.log(team.strStadiumDescription);
+        // for (let i = 0; i < team.length; i++) {
+            let manager = document.createElement('h2');
+            manager.innerText = team.strManager;
+            console.log(team);
+
+            let stadium = document.createElement('p');
+            stadium.innerText = team.strStadium;
+            console.log(team);
+
+            let stadiumDescription = document.createElement('p');
+            stadiumDescription.innerText = team.strStadiumDescription;
+            console.log(team);
 
 
 
@@ -74,43 +68,17 @@ const section = document.querySelector('section');
 
 
 
-
-
-
-
-//             article.appendChild(heading);
-//             heading.appendChild(link);
-//             article.appendChild(img);
-//             article.appendChild(para);
-//             article.appendChild(clearfix);
-//             section.appendChild(article);
+            // let para = document.createElement('p')
+             let dunk = document.createElement('div');
+            // let link = document.createElement('a');
             
-//         }
-//     }
 
-//     if(articles.length === 10) {
-//         nav.style.display = 'block';
-//     } else {
-//         nav.style.display = 'none';
-//     }
-// };
 
-// nav.style.display = 'none';
-
-// function nextPage(e) {
-//     pageNumber++;
-//     fetchResults(e);
-//     console.log("Page number:", pageNumber);
-// }
-
-// function previousPage(e) {
-//     if(pageNumber > 0) {
-//         pageNumber--;
-//     } else {
-//         return;
-//     }
-//     fetchResults(e);
-//     console.log("Page:", pageNumber);
-// };
-
-// const section = document.querySelector('section');
+            dunk.appendChild(manager);
+            // article.appendChild(strStadium);
+            // article.appendChild(strStadiumDescription);
+            // article.appendChild(para);
+            section.appendChild(stadium);
+            section.appendChild(stadiumDescription);
+        }
+    }
